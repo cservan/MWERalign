52c47470daecd9de57cfd6db6dbfa09672255ea1
Christophe SERVAN <christophe.servan@systrangroup.com>
2017-03-01 17:00:24 +0100

    update cbuild

--- cbuild.sh
+++ cbuild.sh
@@ -3,7 +3,7 @@ install -d build
 pushd build
 #cmake -DCMAKE_INSTALL_PREFIX=$HOME/Programmation/TEMP -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 v_uname=`uname`
-/usr/bin/cmake -DCMAKE_EXE_LINKER_FLAGS="-static " -DCMAKE_INSTALL_PREFIX=~/Tools  -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+/usr/bin/cmake -DCMAKE_EXE_LINKER_FLAGS="-static " -DCMAKE_INSTALL_PREFIX=~/Tools/mwerAlign/  -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 make -j4 && \
 make install
 popd
